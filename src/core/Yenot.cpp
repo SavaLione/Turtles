02e0cfb41fad81fb2192e301b39e496db6672d7e
SavaLione <monologuesplus@gmail.com>
2018-04-24 19:16:17 +0500

    Template Matching

--- src/core/Yenot.cpp
+++ src/core/Yenot.cpp
@@ -7,6 +7,11 @@
 #include <Windows.h>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d.hpp>
+
 
 #include <ctime>
 #include <filesystem>
@@ -18,327 +23,93 @@
 using namespace cv;
 using namespace std;
 
-void moments();
-void compare_new();
-
-int main(int argc, char* argv[]) {
-	//settings_initialization();
-
-	//clearning((yenot::database_name + std::string("\\") + yenot::database_file_name), yenot::database_name);
-
-	//databaseAddMember("somebrand");
-
-	//Mat mat_pixel(1, 1, CV_8UC3, Scalar(110, 0, 255));
-
-	//AddMemberPhoto("somebrand", mat_pixel);
-	////
-
-	//moments();
-	compare_new();
-
-	system("pause");
-    return 0;
-}
-
-
-void compare() {
-
-	IplImage *image = 0;
-	image = cvLoadImage("ex.bmp", 1);
-
-	CvMemStorage* storage1 = cvCreateMemStorage(0);
-
-	CvSeq* contours = 0;
-
-	IplImage *image1 = cvCreateImage(cvSize(image->width, image->height), 8, 1); ;
-
-	cvCvtColor(image, image1, CV_BGR2GRAY);
-
-	cvThreshold(image1, image1, 250, 255, CV_THRESH_BINARY);
-
-	cvFindContours(image1, storage1, &contours, sizeof(CvContour), CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cvPoint(0, 0));
-
-
-
-	IplImage* cnt_img = cvCreateImage(cvSize(image->width, image->height), 8, 3);
-
-	CvSeq* _contours = contours;
-
-	_contours->h_next = 0;
-
-	CvSeq* h_next;
-
-	CvSeq* contours1 = _contours->v_next;
-
-	cvShowImage("result1", image1);
-
-	CvMoments moments;
-
-	CvHuMoments HuMoments;
-
-	//Создаём файлы, куда будем записывать моменты
-
-	FILE *f, *f1, *f2;
-
-	f = fopen("data.txt", "w");
-
-	f1 = fopen("data1.txt", "w");
-
-	f2 = fopen("data2.txt", "w");
-
-	char buf[256];
-
-	//Это то, что записано в файле – обучающая выборка
-
-	char alfav[] = { "ZYXWVUTSRQPONMLKJIHGFEDCBA9876543210" };
-
-	int i = 0;
-
-	do {
-
-		cvZero(cnt_img);
-
-		h_next = contours1->h_next;
-
-		contours1->h_next = 0;
-
-		cvDrawContours(cnt_img, contours1, CV_RGB(255, 0, 0), CV_RGB(0, 255, 0), 2, 1, CV_AA, cvPoint(0, 0));
-
-		//считаем моменты здесь
-
-		cvCvtColor(cnt_img, image1, CV_BGR2GRAY);
-
-		//Можно было обойтись без преобразования цвета, пименив binary
-
-		cvMoments(image1, &moments);
-
-		cvGetHuMoments(&moments, &HuMoments);
-
-		//Выводим моменты в файл
-
-		sprintf(buf, "%c:\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t\n", alfav[i], HuMoments.hu1, HuMoments.hu2, HuMoments.hu3
-
-			, HuMoments.hu4, HuMoments.hu5, HuMoments.hu6, HuMoments.hu7);
-
-		fputs(buf, f);
-
-		sprintf(buf, "%c:\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t\n", alfav[i], moments.m00, moments.m01, moments.m02,
-
-			moments.m03, moments.m10, moments.m11, moments.m12, moments.m20, moments.m21, moments.m30);
-
-		fputs(buf, f1);
-
-		sprintf(buf, "%c:\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t\n", alfav[i], moments.mu02, moments.mu03,
-
-			moments.mu11, moments.mu12, moments.mu20, moments.mu21, moments.mu30);
-
-		fputs(buf, f2);
-
-		contours1 = h_next;
-
-		i++;
-
-		if (h_next == NULL) break;
-
-	} while (true);
-
-	fclose(f);
-
-	fclose(f1);
-
-	fclose(f2);
-
-
-
-
-
-	cvReleaseImage(&cnt_img);
-
-	cvReleaseImage(&image);
-
-	cvReleaseImage(&image1);
-
-	cvReleaseMemStorage(&storage1);
-}
-
-void moments123() {
-	Mat img1 = imread("1.png", 1);
-	Mat img2;
-
-	canny(img1, img2);
-
-	CvMoments moments;
-	CvHuMoments HuMoments;
-
-	//cvMoments(img2, &moments);
-
-	cvGetHuMoments(&moments, &HuMoments);
-}
-
-void thresh_callback(const cv::Mat& mat_in, cv::Mat& mat_out);
-/*
-int thresh = 100;
-int max_thresh = 255;
-RNG rng(12345);
-*/
-void moments()
+/// Global Variables
+Mat img; Mat templ; Mat result;
+char* image_window = "Source Image";
+char* result_window = "Result window";
+
+int match_method;
+int max_Trackbar = 5;
+
+/// Function Headers
+void MatchingMethod(int, void*);
+
+/** @function main */
+int main(int argc, char** argv)
 {
+	/// Load image and template
+	img = imread("img.png", 1);
+	templ = imread("templ.png", 1);
 
-	int thresh = 100;
-	int max_thresh = 255;
+	/// Create windows
+	namedWindow(image_window, CV_WINDOW_AUTOSIZE);
+	namedWindow(result_window, CV_WINDOW_AUTOSIZE);
 
-	Mat src = imread("1.png", 1);
-	Mat src_nr;
-	Mat src_done;
-	Mat output;
+	/// Create Trackbar
+	char* trackbar_label = "Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED \n 4: TM COEFF \n 5: TM COEFF NORMED";
+	createTrackbar(trackbar_label, image_window, &match_method, max_Trackbar, MatchingMethod);
 
-	noiseRemoval(src, src_nr);
-	lineDetection(src_nr, src_done);
-
-	thresh_callback(src_done, output);
+	MatchingMethod(0, 0);
 
 	waitKey(0);
+	return 0;
 }
 
-void thresh_callback(const cv::Mat& mat_in, cv::Mat& mat_out)
+/**
+* @function MatchingMethod
+* @brief Trackbar callback
+*/
+void MatchingMethod(int, void*)
 {
-	Mat canny_output = mat_in;
-	vector<vector<Point> > contours;
-	vector<Vec4i> hierarchy;
+	/// Source image to display
+	Mat img_display;
+	img.copyTo(img_display);
 
+	/// Create the result matrix
+	int result_cols = img.cols - templ.cols + 1;
+	int result_rows = img.rows - templ.rows + 1;
 
-	/// Find contours
-	findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+	result.create(result_rows, result_cols, CV_32FC1);
 
-	/// Get the moments
-	vector<Moments> mu(contours.size());
-	for (int i = 0; i < contours.size(); i++)
+	/// Do the Matching and Normalize
+	matchTemplate(img, templ, result, match_method);
+	normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
+
+	/// Localizing the best match with minMaxLoc
+	double minVal; double maxVal; Point minLoc; Point maxLoc;
+	Point matchLoc;
+
+	minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+
+	/// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
+	if (match_method == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED)
 	{
-		mu[i] = moments(contours[i], false);
+		matchLoc = minLoc;
 	}
-
-	/// Calculate the area with the moments 00 and compare with the result of the OpenCV function
-	printf("\t Info: Area and Contour Length \n");
-	for (int i = 0; i< contours.size(); i++)
+	else
 	{
-		printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", i, mu[i].m00, contourArea(contours[i]), arcLength(contours[i], true));
-	}
-	cout << endl << endl << endl << endl;
-	for (int i = 0; i < contours.size(); i++) {
-		cout << "Contour[" << i << "] " << mu[i].m00 << endl;
-	}
-	cout << endl << endl << endl << endl;
-
-	{
-		//cout << mu[].m00;
-	}
-}
-
-void compare_new() {
-	IplImage *src, *templ, *ftmp[6]; //ftmp is what to display on
-	int i;
-
-	// загрузка изображения
-	src = cvLoadImage("1.png", 1);
-
-	// загрузка шаблона
-	templ = cvLoadImage("2.png", 1);
-
-	int patchx = templ->width;
-	int patchy = templ->height;
-	int iwidth = src->width - patchx + 1;
-	int iheight = src->height - patchy + 1;
-	for (i = 0; i<6; ++i) {
-		ftmp[i] = cvCreateImage(cvSize(iwidth, iheight), 32, 1);
+		matchLoc = maxLoc;
 	}
 
-	// сравнение шаблона с изображением
-	for (i = 0; i<6; ++i) {
-		cvMatchTemplate(src, templ, ftmp[i], i);
-		//              double min,max;
-		//              cvMinMaxLoc(ftmp,&min,&max);
-		cvNormalize(ftmp[i], ftmp[i], 1, 0, CV_MINMAX);
-	}
-	// показываем
-	cvNamedWindow("Template");
-	cvShowImage("Template", templ);
-	cvNamedWindow("Image");
-	cvShowImage("Image", src);
+	/// Show me what you got
+	rectangle(img_display, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
+	rectangle(result, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
 
-	cvNamedWindow("SQDIFF");
-	cvShowImage("SQDIFF", ftmp[0]);
-
-	cvNamedWindow("SQDIFF_NORMED");
-	cvShowImage("SQDIFF_NORMED", ftmp[1]);
-
-	cvNamedWindow("CCORR");
-	cvShowImage("CCORR", ftmp[2]);
-
-	cvNamedWindow("CCORR_NORMED");
-	cvShowImage("CCORR_NORMED", ftmp[3]);
-
-	cvNamedWindow("CCOEFF");
-	cvShowImage("CCOEFF", ftmp[4]);
-
-	cvNamedWindow("CCOEFF_NORMED");
-	cvShowImage("CCOEFF_NORMED", ftmp[5]);
-
-	// ждём нажатия клавиши
-	cvWaitKey(0);
+	imshow(image_window, img_display);
+	imshow(result_window, result);
 }
 
 /*
-char* image = argc == 2 ? argv[1] : "a.png";
+//settings_initialization();
 
-cv::Mat out;
-cv::Mat mat_image = cv::imread(image, 1);
+//clearning((yenot::database_name + std::string("\\") + yenot::database_file_name), yenot::database_name);
 
-settings_initialization();
+//databaseAddMember("somebrand");
 
-noiseRemoval(mat_image, out);
-mat_image = out;
+//Mat mat_pixel(1, 1, CV_8UC3, Scalar(110, 0, 255));
 
-lineDetection(mat_image, out);
-mat_image = out;
-*/
+//AddMemberPhoto("somebrand", mat_pixel);
+////
 
-/*
-
-cv::Mat mat_image = cv::imread("a.png", 1);
-cv::Mat mat_out;
-
-lineDetection(mat_image, mat_out);
-mat_image = mat_out;
-
-//FileStorage fs("example_001.xml", FileStorage::WRITE);
-//fs << "image" << mat_image;
-//fs << "some" << mat_image;
-//fs.release();
-
-//FileStorage fs;
-//fs.open("example_001.xml", FileStorage::READ);
-//fs["image"] >> mat_out;
-//imwrite("new.png", mat_out);
-
-std::vector<string> stringVector;
-//stringVector.insert(stringVector.end(), "");
-
-FileStorage fs("example.xml", FileStorage::WRITE);
-fs << "stringVector" << stringVector;
-fs.release();
-
-std::vector<string> stringVectorNew;
-FileStorage fsNew;
-fsNew.open("example.xml", FileStorage::READ);
-fsNew["stringVector"] >> stringVectorNew;
-
-stringVectorNew.insert(stringVector.end(), "SOMETHING");
-
-FileStorage fsNew1("example.xml", FileStorage::WRITE);
-fsNew1 << "stringVector" << stringVectorNew;
-fsNew1.release();
-*/
-
-/*
-Mat mat_pixel(1,1, CV_8UC3, Scalar(110,0,255));
+//moments();
 */
