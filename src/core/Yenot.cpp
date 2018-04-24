033b414bf6b1739cdb9609a56acd7bac875cd475
SavaLione <monologuesplus@gmail.com>
2018-04-24 20:05:34 +0500

    Lib update

--- src/core/Yenot.cpp
+++ src/core/Yenot.cpp
@@ -5,12 +5,13 @@
 */
 #include <iostream>
 #include <Windows.h>
-#include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/core/core.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/features2d.hpp>
+#include <opencv2\calib3d\calib3d.hpp>
+#include <opencv2\nonfree\nonfree.hpp>
 
 
 #include <ctime>
@@ -23,80 +24,114 @@
 using namespace cv;
 using namespace std;
 
-/// Global Variables
-Mat img; Mat templ; Mat result;
-char* image_window = "Source Image";
-char* result_window = "Result window";
-
-int match_method;
-int max_Trackbar = 5;
-
-/// Function Headers
-void MatchingMethod(int, void*);
+void readme();
 
 /** @function main */
 int main(int argc, char** argv)
 {
-	/// Load image and template
-	img = imread("img.png", 1);
-	templ = imread("templ.png", 1);
+	if (argc != 3)
+	{
+		readme(); return -1;
+	}
 
-	/// Create windows
-	namedWindow(image_window, CV_WINDOW_AUTOSIZE);
-	namedWindow(result_window, CV_WINDOW_AUTOSIZE);
+	Mat img_object = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+	Mat img_scene = imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
 
-	/// Create Trackbar
-	char* trackbar_label = "Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED \n 4: TM COEFF \n 5: TM COEFF NORMED";
-	createTrackbar(trackbar_label, image_window, &match_method, max_Trackbar, MatchingMethod);
+	if (!img_object.data || !img_scene.data)
+	{
+		std::cout << " --(!) Error reading images " << std::endl; return -1;
+	}
 
-	MatchingMethod(0, 0);
+	//-- Step 1: Detect the keypoints using SURF Detector
+	int minHessian = 400;
+
+	SurfFeatureDetector detector(minHessian);
+
+	std::vector<KeyPoint> keypoints_object, keypoints_scene;
+
+	detector.detect(img_object, keypoints_object);
+	detector.detect(img_scene, keypoints_scene);
+
+	//-- Step 2: Calculate descriptors (feature vectors)
+	SurfDescriptorExtractor extractor;
+
+	Mat descriptors_object, descriptors_scene;
+
+	extractor.compute(img_object, keypoints_object, descriptors_object);
+	extractor.compute(img_scene, keypoints_scene, descriptors_scene);
+
+	//-- Step 3: Matching descriptor vectors using FLANN matcher
+	FlannBasedMatcher matcher;
+	std::vector< DMatch > matches;
+	matcher.match(descriptors_object, descriptors_scene, matches);
+
+	double max_dist = 0; double min_dist = 100;
+
+	//-- Quick calculation of max and min distances between keypoints
+	for (int i = 0; i < descriptors_object.rows; i++)
+	{
+		double dist = matches[i].distance;
+		if (dist < min_dist) min_dist = dist;
+		if (dist > max_dist) max_dist = dist;
+	}
+
+	printf("-- Max dist : %f \n", max_dist);
+	printf("-- Min dist : %f \n", min_dist);
+
+	//-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
+	std::vector< DMatch > good_matches;
+
+	for (int i = 0; i < descriptors_object.rows; i++)
+	{
+		if (matches[i].distance < 3 * min_dist)
+		{
+			good_matches.push_back(matches[i]);
+		}
+	}
+
+	Mat img_matches;
+	drawMatches(img_object, keypoints_object, img_scene, keypoints_scene,
+		good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+		vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+
+	//-- Localize the object
+	std::vector<Point2f> obj;
+	std::vector<Point2f> scene;
+
+	for (int i = 0; i < good_matches.size(); i++)
+	{
+		//-- Get the keypoints from the good matches
+		obj.push_back(keypoints_object[good_matches[i].queryIdx].pt);
+		scene.push_back(keypoints_scene[good_matches[i].trainIdx].pt);
+	}
+
+	Mat H = findHomography(obj, scene, CV_RANSAC);
+
+	//-- Get the corners from the image_1 ( the object to be "detected" )
+	std::vector<Point2f> obj_corners(4);
+	obj_corners[0] = cvPoint(0, 0); obj_corners[1] = cvPoint(img_object.cols, 0);
+	obj_corners[2] = cvPoint(img_object.cols, img_object.rows); obj_corners[3] = cvPoint(0, img_object.rows);
+	std::vector<Point2f> scene_corners(4);
+
+	perspectiveTransform(obj_corners, scene_corners, H);
+
+	//-- Draw lines between the corners (the mapped object in the scene - image_2 )
+	line(img_matches, scene_corners[0] + Point2f(img_object.cols, 0), scene_corners[1] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
+	line(img_matches, scene_corners[1] + Point2f(img_object.cols, 0), scene_corners[2] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
+	line(img_matches, scene_corners[2] + Point2f(img_object.cols, 0), scene_corners[3] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
+	line(img_matches, scene_corners[3] + Point2f(img_object.cols, 0), scene_corners[0] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
+
+	//-- Show detected matches
+	imshow("Good Matches & Object detection", img_matches);
 
 	waitKey(0);
 	return 0;
 }
 
-/**
-* @function MatchingMethod
-* @brief Trackbar callback
-*/
-void MatchingMethod(int, void*)
+/** @function readme */
+void readme()
 {
-	/// Source image to display
-	Mat img_display;
-	img.copyTo(img_display);
-
-	/// Create the result matrix
-	int result_cols = img.cols - templ.cols + 1;
-	int result_rows = img.rows - templ.rows + 1;
-
-	result.create(result_rows, result_cols, CV_32FC1);
-
-	/// Do the Matching and Normalize
-	matchTemplate(img, templ, result, match_method);
-	normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
-
-	/// Localizing the best match with minMaxLoc
-	double minVal; double maxVal; Point minLoc; Point maxLoc;
-	Point matchLoc;
-
-	minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
-
-	/// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
-	if (match_method == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED)
-	{
-		matchLoc = minLoc;
-	}
-	else
-	{
-		matchLoc = maxLoc;
-	}
-
-	/// Show me what you got
-	rectangle(img_display, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
-	rectangle(result, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
-
-	imshow(image_window, img_display);
-	imshow(result_window, result);
+	std::cout << " Usage: ./SURF_descriptor <img1> <img2>" << std::endl;
 }
 
 /*
