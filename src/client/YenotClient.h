5620339d46431752bb6dbc411b76b6197562785c
SavaLione <monologuesplus@gmail.com>
2018-05-09 17:36:34 +0500

    Client-server

---
+++ src/client/YenotClient.h
@@ -0,0 +1,16 @@
+/**
+	@file
+	@brief 
+	@author SavaLione
+	@date 20 Apr 2018
+*/
+/**
+	@defgroup serveryenotserverh YenotServer.h
+	@ingroup server
+	@{
+*/
+#ifndef YENOTCLIENT_H
+#define YENOTCLIENT_H
+
+#endif // YENOTCLIENT_H
+/** @} */
